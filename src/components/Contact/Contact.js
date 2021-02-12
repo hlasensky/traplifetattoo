aa717d7f91299470cf8adc68df1b32a36423ec2e
Drizz159 <celnar.lukas@gmail.com>
2021-02-12 23:37:13 +0100

    Finishing scss and jsx in Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,23 +6,17 @@ const Contact = () => {
         <div className='contact'>
             <h1 className='contact__header'>JAK MĚ KONTAKTOVAT?</h1>
 
-
-
-
             <div className='contact__main'>
-
-
-
                 <iframe className='contact__main-container' src="https://maps.google.com/maps?q=Osv%C4%9Btim&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
                 <div className='contact__main-container'>
                     <img className='contact__main-polygon' src='/images/polygon.png' alt='polygon'/>
                     <form className='contact__main-form'>
                         <div className='contact__main-form-header'>EMAIL</div>
                         <div className='contact__main-form-inputs'>
-                            <input className='contact__main-form-input' type='text' /><br />
-                            <input className='contact__main-form-input' type='email' /><br />
-                            <textarea className='contact__main-form-textarea' style={{resize: 'none'}}></textarea><br />
-                            <input className='contact__main-form-submit' type='submit' />
+                            <input className='contact__main-form-input' placeholder='CELÉ JMÉNO' type='text' /><br />
+                            <input className='contact__main-form-input' type='email' placeholder='EMAIL' /><br />
+                            <textarea className='contact__main-form-textarea' placeholder='ZPRÁVA' style={{resize: 'none'}}></textarea><br />
+                            <div className='contact__main-form-submit'>ODESLAT</div>
                         </div>
                     </form>
                 </div>
@@ -35,14 +29,12 @@ const Contact = () => {
 
             <div className='contact__socials'>
                 <div className='contact__socials-container'>
-                    <div className='contact__socials-header'>INSTAGRAM</div>
                     <a href='https://www.instagram.com/traplifetattoo/' className='contact__socials-content'>
                         <img className='contact__socials-content-icon' src='/images/ig-icon.png' alt='ig-icon'/>
                         <div className='contact__socials-content-link'>traplifetattoo</div>
                     </a>
                 </div>
                 <div className='contact__socials-container contact__socials-container-last'>
-                    <div className='contact__socials-header'>FACEBOOK</div>
                     <a href='https://www.facebook.com/traphousetattoobrno' className='contact__socials-content'>
                         <img className='contact__socials-content-icon' src='/images/fb-icon.png' alt='fb-icon'/>
                         <div className='contact__socials-content-link'>traplifetattoo</div>
